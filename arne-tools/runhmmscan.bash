895fa7299c039de7986127b63e0255af24e05f3e
Arne Elofsson at Abisko <arne@bioinfo.se>
2015-08-26 01:36:37 +0200

    By syncing

--- arne-tools/runhmmscan.bash
+++ arne-tools/runhmmscan.bash
@@ -2,7 +2,7 @@
 
 for k in $*
 do
-    find $k -type f -name "*.fa" -exec  sh -c ' j=`basename {} .fa` ; i=`dirname {}` ; if [ ! -e $i/$j.pfamout ] ; then hmmscan --cpu 1 --domtblout $i/$j.domtblout -o  $i/$j.pfamout /pfs/nobackup/home/a/arnee/FastPSSM/data/Pfam-28/Pfam-A.hmm  {} ; else  echo $j ; fi ' \; 
+    find $k -type f -name "*.fa" -exec  sh -c ' j=`basename {} .fa` ; i=`dirname {}` k=`echo $i| sed s/data/out/` ; l=`echo $i| sed s/data/results/`; if [ ! -e $k/$j.pfamout ] ; then hmmscan --cpu 1 --domtblout $l/$j.domtblout -o  $k/$j.pfamout /pfs/nobackup/home/a/arnee/FastPSSM/data/Pfam-28/Pfam-A.hmm  {} ; else  echo $j ; fi ' \; 
 done
 
 
