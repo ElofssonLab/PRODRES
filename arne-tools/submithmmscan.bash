6d7e30c427f31f6a4c02090138d296902afdaaaf
Arne Elofsson at Abisko <arne@bioinfo.se>
2015-08-26 22:54:38 +0200

    hej

--- arne-tools/submithmmscan.bash
+++ arne-tools/submithmmscan.bash
@@ -1,6 +1,7 @@
 #!/bin/bash 
 #SBATCH -A snic2015-10-12
-# We actually start 6 jobs in parallel. Probably more efficient than running with 5 thread.
+# We actually start 6 jobs in parallel. 
+# Probably more efficient than running with 5 thread.
 #SBATCH -n 6
 #SBATCH -c 1
 #SBATCH --time=24:00:00
